f0429f4b2de365e3c878c2423d9b02d05b81987a
Kiki <kiki@ebconnect.com>
2025-08-15 22:55:19 +0700

    feat(ci,build,tofu): Introduce dynamic environment and manual CD trigger
    - Dynamically determine OpenTofu environment and variables based on git
    branch for `tofu init` and `tofu plan`.
    - Standardize OpenTofu backend state prefix in `cloudbuild.yaml` and
    OpenTofu scripts using application name and service.
    - Add `manual-trigger-cd-pipeline.sh` for simplified manual continuous
    deployment triggering.
    - Update Cloud Build configurations to leverage new dynamic application
    name and service substitutions.

--- scripts/tofu-init.sh
+++ scripts/tofu-init.sh
@@ -1,2 +1,12 @@
 #/bin/bash
-tofu init -var-file="environments/prd/terraform.tfvars" -backend-config="bucket=khhini-devops-2705-bkt" -backend-config="prefix=tofu/sample-app/prd"
+_APP_NAME=sample-app
+_APP_SERVICE=backend
+_TOFU_BACKEND_BUCKET=khhini-devops-2705-bkt
+
+_TOFU_BACKEND_PREFIX=tofu/${_APP_NAME}/${_APP_SERVICE}
+
+_build_env=$(git rev-parse --abbrev-ref HEAD)
+if [ "${_build_env}" = "main" ]; then
+  _build_env=prd
+fi
+tofu init -var-file="environments/${_build_env}/terraform.tfvars" -backend-config="bucket=${_TOFU_BACKEND_BUCKET}" -backend-config="prefix=${_TOFU_BACKEND_PREFIX}/${_build_env}" -migrate-state
--- scripts/tofu-plan.sh
+++ scripts/tofu-plan.sh
@@ -1,2 +1,7 @@
 #/bin/bash
-tofu plan -var-file="environments/prd/terraform.tfvars" -out="./tmp/environment-prd.plan" -var=app_image_tag=hello
+_app_image_tag=$(tofu output app_image_tag | tr -d '"')
+_build_env=$(git rev-parse --abbrev-ref HEAD)
+if [ "${_build_env}" = "main" ]; then
+  _build_env=prd
+fi
+tofu plan -var-file="environments/${_build_env}/terraform.tfvars" -out="./tmp/environment-${_build_env}.plan" -var=app_image_tag=${_app_image_tag}
