c835af87d5feadb99ae0beda606292dcfa11c0da
Kiki <kiki@ebconnect.com>
2025-08-19 23:32:59 +0700

    feat(scripts): update manual triggers config

--- scripts/manual-trigger-cd-pipeline.sh
+++ scripts/manual-trigger-cd-pipeline.sh
@@ -1,17 +1,26 @@
 #/bin/bash
 _APP_NAME=sample-app
 _APP_SERVICE=backend
-_TOFU_BACKEND_BUCKET=ebc-devops-bkt
+_TOFU_BACKEND_BUCKET=$(gcloud config get-value project)-bkt
 _DEPLOYMENT_CONFIG_PATH=./deployments
-LOCATION=asia-southeast2
+LOCATION=asia-east1
 
 _cloudbuild_cd_trigger=$(tofu output continuous_deployment_trigger | tr -d '"')
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 COMMIT_SHA=$(tofu output app_image_tag | tr -d '"')
 _TOFU_BACKEND_PREFIX=tofu/${_APP_NAME}/${_APP_SERVICE}
 
+echo "Running..."
+echo "=========================================="
+echo "gcloud builds triggers run ${_cloudbuild_cd_trigger} \
+  --branch=${BRANCH_NAME} \
+  --substitutions=_DEPLOYMENT_CONFIG_PATH=${_DEPLOYMENT_CONFIG_PATH},_TOFU_BACKEND_BUCKET=${_TOFU_BACKEND_BUCKET},_TOFU_BACKEND_PREFIX=${_TOFU_BACKEND_PREFIX},_APP_IMAGE_TAG=$COMMIT_SHA \
+  --project=$(gcloud config get-value project) \
+  --region=${LOCATION}"
+echo "=========================================="
+
 gcloud builds triggers run ${_cloudbuild_cd_trigger} \
   --branch=${BRANCH_NAME} \
   --substitutions=_DEPLOYMENT_CONFIG_PATH=${_DEPLOYMENT_CONFIG_PATH},_TOFU_BACKEND_BUCKET=${_TOFU_BACKEND_BUCKET},_TOFU_BACKEND_PREFIX=${_TOFU_BACKEND_PREFIX},_APP_IMAGE_TAG=$COMMIT_SHA \
-  --project=ebc-source-code \
+  --project=$(gcloud config get-value project) \
   --region=${LOCATION}
