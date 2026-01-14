ab916b691c69a7ac839927d5d6fdfd01490096ea
Kiki <kiki@ebconnect.com>
2026-01-14 16:55:28 +0700

    chores: refactoring scripts for only cloudrun deployment

--- terraforms/providers.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-  # Configuration options
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
-}
--- terraforms/scripts/tofu-plan.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-_app_image_tag=$(tofu output app_image_tag | tr -d '"')
-_build_env=$(git rev-parse --abbrev-ref HEAD)
-if [ "${_build_env}" = "main" ]; then
-  _build_env=prd
-fi
-tofu plan -var-file="environments/${_build_env}/terraform.tfvars" -out="./tmp/environment-${_build_env}.plan" -var=app_image_tag=${_app_image_tag}
